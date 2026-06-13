0e922bae8e00aed2a4b4318e0089ad8fc8cea907
Wyatt Stanke <wyattstanke@gmail.com>
2026-06-13 12:49:16 -0400

    Add dek and hidden

--- astro/src/content.config.ts
+++ astro/src/content.config.ts
@@ -9,11 +9,13 @@ const blog = defineCollection({
 	schema: ({ image }) =>
 		z.object({
 			title: z.string(),
+			dek: z.string().optional(),
 			description: z.string(),
 			// Transform string to Date object
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
 			heroImage: z.optional(image()),
+			hidden: z.boolean().optional(),
 		}),
 });
 
